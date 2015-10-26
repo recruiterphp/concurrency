bcb41034a90511511b6c784ad901c77708918c0a
Giorgio Sironi <giorgio.sironi@onebip.com>
2015-10-26 12:27:21 +0100

    MongoLock class

---
+++ src/Onebip/Concurrency/MongoLock.php
@@ -0,0 +1,128 @@
+<?php
+namespace Onebip\Concurrency;
+use MongoCollection;
+use MongoCursorException;
+use MongoDate;
+use DateTime;
+use DateTimeZone;
+use DateInterval;
+use Onebip\Clock;
+use Onebip\Clock\SystemClock;
+
+class MongoLock implements Lock
+{
+    private $collection;
+    private $processName;
+    const DUPLICATE_KEY = 11000;
+
+    public function __construct(MongoCollection $collection, $programName, $processName, $clock = null, $sleep = 'sleep')
+    {
+        $this->collection = $collection;
+        $this->collection->ensureIndex(['program' => 1], ['unique' => true]);
+        $this->programName = $programName;
+        $this->processName = $processName;
+        if ($clock === null) {
+            $clock = new SystemClock();
+        }
+        $this->clock = $clock;
+        $this->sleep = $sleep;
+    }
+
+    public static function forProgram($programName, MongoCollection $collection)
+    {
+        return new self($collection, $programName, gethostname() . ':' . getmypid());
+    }
+
+    public function acquire($duration = 3600)
+    {
+        $now = $this->clock->current();
+
+        $this->removeExpiredLocks($now);
+
+        $expiration = clone $now;
+        $expiration->add(new DateInterval("PT{$duration}S"));
+
+        try {
+            $this->collection->insert([
+                'program' => $this->programName,
+                'process' => $this->processName,
+                'acquired_at' => new MongoDate($now->getTimestamp()),
+                'expires_at' => new MongoDate($expiration->getTimestamp()),
+            ]);
+        } catch (MongoCursorException $e) {
+            if ($e->getCode() == self::DUPLICATE_KEY) {
+                throw new LockNotAvailableException("{$this->processName} cannot acquire a lock for the program {$this->programName}");
+            }
+            throw $e;
+        }
+    } 
+
+    public function show()
+    {
+        $document = $this->collection->findOne(
+            ['program' => $this->programName]
+        );
+        $this->convertToIso8601String($document['acquired_at']);
+        $this->convertToIso8601String($document['expires_at']);
+        unset($document['_id']);
+        return $document;
+    }
+
+    public function release($force = false)
+    {
+        $query = ['program' => $this->programName];
+        if (!$force) {
+            $query['process'] = $this->processName;
+        }
+        $operationResult = $this->collection->remove($query);
+        $affectedDocuments = $operationResult['n'];
+        if ($affectedDocuments != 1) {
+            throw new LockNotAvailableException("{$this->processName} does not have a lock for {$this->programName} to release");
+        }
+    }
+
+    /**
+     * @param integer $polling  how frequently to check the lock presence
+     * @param integer $maximumWaitingTime  a limit to the waiting
+     */
+    public function wait($polling = 30, $maximumWaitingTime = 3600)
+    {
+        $i = 0;
+        $timeLimit = $this->clock->current()->add(new DateInterval("PT{$maximumWaitingTime}S"));
+        while (true) {
+            $now = $this->clock->current();
+            $result = $this->collection->count($query = [
+                'program' => $this->programName,
+                'expires_at' => ['$gt' => new MongoDate($now->getTimestamp())],
+            ]);
+
+            $i++;
+            if ($result) {
+                if ($now > $timeLimit) {
+                    throw new LockNotAvailableException("I have been waiting up until {$timeLimit->format(DateTime::ISO8601)} for the lock $this->programName ($maximumWaitingTime seconds), but it is still not available.");
+                }
+                call_user_func($this->sleep, $polling);
+            } else {
+                break;
+            }
+        }
+    }
+
+    private function removeExpiredLocks(DateTime $now)
+    {
+        $this->collection->remove($query = [
+            'program' => $this->programName,
+            'expires_at' => [
+                '$lt' => new MongoDate($now->getTimestamp()),
+            ],
+        ]);
+    }
+
+    private function convertToIso8601String(&$field)
+    {
+        $datetime = new DateTime();
+        $datetime->setTimestamp($field->sec);
+        $datetime->setTimezone(new DateTimeZone('UTC'));
+        $field = $datetime->format(DateTime::ISO8601);
+    }
+}

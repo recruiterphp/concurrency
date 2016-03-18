5cff7cace3a26466f57c069f3b3bbfd4f860dd95
Giorgio Sironi <work@giorgiosironi.com>
2016-03-18 10:30:17 +0100

    Main method for using a custom model and history

--- knossos-onebip/src/knossos_onebip/core.clj
+++ knossos-onebip/src/knossos_onebip/core.clj
@@ -1,7 +1,30 @@
 (ns knossos-onebip.core
-  (:gen-class))
+  (:gen-class)
+  (:require [knossos.core :as core]
+            [knossos-onebip.model :as model]
+            [clojure.java.io :as io]
+            [clojure-csv.core :refer [parse-csv]]))
+
+(defn history-from-file [path]
+  (let [lines (-> (io/file path)
+                  io/reader
+                  parse-csv)]
+    (map (fn [[_ process type f]]
+           {:type (keyword type)
+            :process (keyword process)
+            :f (keyword f)
+            :value nil})
+         lines)))
+
+(defn check-history [model-fn filename]
+  (let [lock (model-fn)
+        history (history-from-file filename)]
+    (core/analysis lock history)))
 
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Pass a model name (e.g. 'mongo-lock' and a CSV file.
+  with the structure 'timestamp,process,type,f' (e.g. '1458232058222295,pp0,invoke|ok|fail,acquire')"
   [& args]
-  (println "Hello, World!"))
+  (let [[model-name filename] args]
+    (check-history (ns-resolve (find-ns 'knossos-onebip.model) (symbol model-name))
+                    filename)))
--- knossos-onebip/test/knossos_onebip/model_test.clj
+++ knossos-onebip/test/knossos_onebip/model_test.clj
@@ -2,11 +2,8 @@
   (:require [clojure.test :refer :all]
             [knossos-onebip.model :refer :all]
             [knossos.model :refer :all]
-            [knossos.core :as core]
             [knossos.op :refer :all]
-            [clojure.java.io :as io]
-            [clojure.pprint :refer [pprint]]
-            [clojure-csv.core :refer [parse-csv]]))
+            [clojure.pprint :refer [pprint]]))
 
 (deftest free-state
   (let [lock (mongo-lock)]
@@ -26,21 +23,5 @@
     (is (= (inconsistent "acquired a taken lock")
            (step lock (ok :p2 :acquire nil))))))
 
-(defn history-from-file [path]
-  (let [lines (-> (io/file path)
-                  io/reader
-                  parse-csv)]
-    (map (fn [[_ process type f]]
-           {:type (keyword type)
-            :process (keyword process)
-            :f (keyword f)
-            :value nil})
-         lines)))
-
-(defn sample-history []
-  (let [lock (mongo-lock)
-        history
-          (history-from-file "/home/giorgio/provisioning2/mongolock_98.log")]
-    (core/analysis lock history)))
 
 ; does this reorder the operations of a single process?

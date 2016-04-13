684d6c33504eb420eb05aa9e172ff20c42f0e023
MirkoBonadei <mirko.bonadei@gmail.com>
2016-04-13 16:45:56 +0200

    Adding defaults to the Lock interface

--- src/Onebip/Concurrency/Lock.php
+++ src/Onebip/Concurrency/Lock.php
@@ -7,18 +7,18 @@ interface Lock
      * @throws LockNotAvailableException
      * @param $duration (in seconds)
      */
-    public function acquire($duration);
+    public function acquire($duration = 360);
 
     /**
      * @param $force boolean
      */
-    public function release($force);
+    public function release($force = false);
 
     /**
      * @throws LockNotavailableexception
      * @param $duration (in seconds)
      */
-    public function refresh($duration);
+    public function refresh($duration = 3600);
 
     /**
      * @return array  diagnostic information
@@ -30,5 +30,5 @@ interface Lock
      * @param $maximumWaitingTime (in seconds)
      * @return void
      */
-    public function wait($polling, $maximumWaitingTime);
+    public function wait($polling = 30, $maximumWaitingTime = 3600);
 }

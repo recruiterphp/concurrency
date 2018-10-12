f5b61655289ade102f20bc4dd392ab7530a1ee24
D. Bellettini <325358+dbellettini@users.noreply.github.com>
2018-10-12 10:55:46 +0000

    Use return type hinting

--- src/Onebip/Concurrency/NullLock.php
+++ src/Onebip/Concurrency/NullLock.php
@@ -8,14 +8,14 @@ class NullLock implements Lock
      * @throws LockNotAvailableException
      * @param $duration (in seconds)
      */
-    public function acquire($duration = 360)
+    public function acquire($duration = 360): void
     {
     }
 
     /**
      * @param $force boolean
      */
-    public function release($force = false)
+    public function release($force = false): void
     {
     }
 
@@ -23,11 +23,11 @@ class NullLock implements Lock
      * @throws LockNotavailableexception
      * @param $duration (in seconds)
      */
-    public function refresh($duration = 3600)
+    public function refresh($duration = 3600): void
     {
     }
 
-    public function show()
+    public function show(): array
     {
         return [
             'program' => 'null-lock',
@@ -41,7 +41,7 @@ class NullLock implements Lock
      * @param $maximumWaitingTime (in seconds)
      * @return void
      */
-    public function wait($polling = 30, $maximumWaitingTime = 3600)
+    public function wait($polling = 30, $maximumWaitingTime = 3600): void
     {
     }
 }

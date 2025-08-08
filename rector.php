80db7802dd028069c33d9b54e7e2fc2edba2535c
Davide Bellettini <325358+dbellettini@users.noreply.github.com>
2025-08-09 00:35:42 +0200

    Add more rector sets

--- rector.php
+++ rector.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
+use Rector\DeadCode\Rector\Plus\RemoveDeadZeroAndOneOperationRector;
 
 return RectorConfig::configure()
     ->withPaths([
@@ -11,4 +12,9 @@ return RectorConfig::configure()
     ])
     // uncomment to reach your current PHP version
     ->withPhpSets(php84: true)
-    ->withPreparedSets(deadCode: true, codeQuality: true, typeDeclarations: true);
+    ->withPreparedSets(
+        deadCode: true,
+        codeQuality: true,
+        typeDeclarations: true,
+        phpunitCodeQuality: true
+    )->withSkip([RemoveDeadZeroAndOneOperationRector::class]);
